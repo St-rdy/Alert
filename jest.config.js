df22601bc247eea14e53d463dd5b7cb1126900a9
sehyun lee <mh080411@kyonggi.ac.kr>
2026-01-30 13:51:49 +0900

    [BACKEND-20] Initialize Alert Service

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/*.test.ts",
+    "!src/**/*.spec.ts",
+    "!src/server.ts",
+  ],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleFileExtensions: ["ts", "js", "json"],
+  verbose: true,
+};
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import app from "./app";
+
+app.listen(3000, () => {
+  console.log("서버 실행 중");
+});
